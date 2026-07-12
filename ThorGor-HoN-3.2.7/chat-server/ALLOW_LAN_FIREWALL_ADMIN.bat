0b524ff65390df31e3be55ace5c628c4f6fea845
lightningfastcomputing <lightningfastcomputing@gmail.com>
2026-07-12 12:05:02 -0400

    Delete ThorGor-HoN-3.2.7 directory

--- ThorGor-HoN-3.2.7/chat-server/ALLOW_LAN_FIREWALL_ADMIN.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo This must be run as Administrator.
-netsh advfirewall firewall add rule name="ThorGor HoN Chat TCP 11031" dir=in action=allow protocol=TCP localport=11031 profile=private
-pause
